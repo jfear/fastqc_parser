eb6bdb623b785b3f668bf5ea3326b92cda869387
Justin Fear <justin.fear@nih.gov>
2016-05-02 14:06:09 -0400

    Adds script to installer and fixes readme

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ requirements = open(os.path.join(os.path.dirname(__file__), 'requirements.txt'))
 
 setup(
     name = 'fastqc_parser',
-    version = '1.0.0beta',
+    version = '1.0.1beta',
     author = 'Justin Fear',
     author_email = 'justin.m.fear@gmail.com',
     description = 'A small utility for parsing FASTQC output',
     install_requires = requirements,
     packages=['fastqc_parser'],
     package_dir={'fastqc_parser': 'fastqc_parser'},
+    scripts=['bin/blastFastQC'],
 )
